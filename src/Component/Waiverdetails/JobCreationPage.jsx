0edc24d00c3be6334a6b2a95f949539a4f768184
RajamaniT <rajamanitmca@gmail.com>
2024-05-14 12:18:34 +0530

    design changes in job creation page

--- src/Component/Waiverdetails/JobCreationPage.jsx
+++ src/Component/Waiverdetails/JobCreationPage.jsx
@@ -37,7 +37,7 @@ function JobCreationPage() {
     enddate: {
       required: "End date is required",
       validate: (value) =>
-        value <= watch("startdate") || "Password do not match",
+        value <= watch("startdate") || "Start date should higher than end date",
     },
   };
 
@@ -63,148 +63,139 @@ function JobCreationPage() {
   const [enddate, setEndDate] = useState(new Date());
 
   return (
-    <div className="container d-flex flex-column">
-      <div className="d-flex">
-        <div className="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
-          <div className="d-table-cell align-middle">
-            <div className="card">
-              <div className="card-body shadow-lg bg-White rounded">
-                <div className="m-sm-3">
-                  <div className="text-center col-12">
-                    <h3 className="text-primary">
-                      <strong>Job Creation</strong>
-                    </h3>
+    <div className="container ">
+      <div className="d-flex justify-content-center align-content-center">
+        <div className="col-md-10">
+          <div className="shadow-lg bg-White rounded m-5">
+            <div className="text-center col-12 align-content-center">
+              <h3 className="text-primary m-3">
+                <strong>Job Creation</strong>
+              </h3>
+            </div>
+            <form className="p-4" onSubmit={handleSubmit(onSubmit)}>
+              <hr className="bg-danger border-2 border-top border-primary mb-4"></hr>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <div className="row mb-3">
+                    <Form.Group controlId="waivername">
+                      <Form.Label>Waiver Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter waiver name"
+                        {...register("waivername", registerOption.email)}
+                      />
+                      {errors.waivername && (
+                        <p className="errorMsg">{errors.waivername.message}</p>
+                      )}
+                    </Form.Group>
                   </div>
-                  <form className="" onSubmit={handleSubmit(onSubmit)}>
-                    <hr className="bg-danger border-2 border-top border-primary mb-5"></hr>
-                    <div className="row mb-3">
-                      <div className="col-md-12">
-                        <Form.Group controlId="waivername">
-                          <Form.Label>Waiver Name</Form.Label>
-                          <Form.Control
-                            type="text"
-                            placeholder="Enter waiver name"
-                            {...register("waivername", registerOption.email)}
+                  <div className="row mb-3">
+                    <div className="col-md-6">
+                      <Form.Label>Fabric Type</Form.Label>
+                      <Controller
+                        className="mb-3"
+                        name="fabric"
+                        control={control}
+                        render={({ onChange, value, ref }) => (
+                          <Select
+                            options={fabric}
+                            value={fabric.find((c) => c.value === value)}
+                            // onChange={(val) => onChange(val.value)}
                           />
-                          {errors.waivername && (
-                            <p className="errorMsg">
-                              {errors.waivername.message}
-                            </p>
-                          )}
-                        </Form.Group>
-                      </div>
-                    </div>
-
-                    <div className="row mb-3">
-                      <div className="col-md-6">
-                        <label>Fabric Type</label>
-                        <Controller
-                          className="mb-3"
-                          name="fabric"
-                          control={control}
-                          render={({ onChange, value, ref }) => (
-                            <Select
-                              options={fabric}
-                              value={fabric.find((c) => c.value === value)}
-                              // onChange={(val) => onChange(val.value)}
-                            />
-                          )}
-                          rules={{ required: true }}
-                        />
-                        {errors.fabric && (
-                          <p className="errorMsg">{errors.fabric.message}</p>
                         )}
-                      </div>
-                      <div className="col-md-6">
-                        <label>Yarn details</label>
-                        <Controller
-                          className="mb-3"
-                          name="yarn"
-                          control={control}
-                          render={({ onChange, value, ref }) => (
-                            <Select
-                              options={yarn}
-                              value={yarn.find((c) => c.value === value)}
-                              // onChange={(val) => onChange(val.value)}
-                            />
-                          )}
-                          rules={{ required: true }}
-                        />
-                        {errors.yarn && (
-                          <p className="errorMsg">{errors.yarn.message}</p>
-                        )}
-                      </div>
+                        rules={{ required: true }}
+                      />
+                      {errors.fabric && (
+                        <p className="errorMsg">{errors.fabric.message}</p>
+                      )}
                     </div>
-
-                    <Form.Group className="mb-3" controlId="designdetails">
+                    <div className="col-md-6">
+                      <Form.Label>Yarn details</Form.Label>
+                      <Controller
+                        className="mb-3"
+                        name="yarn"
+                        control={control}
+                        render={({ onChange, value, ref }) => (
+                          <Select
+                            options={yarn}
+                            value={yarn.find((c) => c.value === value)}
+                            // onChange={(val) => onChange(val.value)}
+                          />
+                        )}
+                        rules={{ required: true }}
+                      />
+                      {errors.yarn && (
+                        <p className="errorMsg">{errors.yarn.message}</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="row mb-3">
+                    <div className="col-md-6">
+                      <Form.Label>Start date</Form.Label>
+                      <Form.Control
+                        type="date"
+                        name="startdate"
+                        placeholder="DateRange"
+                        value={startdate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                      />
+                      {errors.startdate && (
+                        <p className="errorMsg">{errors.startdate.message}</p>
+                      )}
+                    </div>
+                    <div className="col-md-6">
+                      <Form.Label>End date</Form.Label>
+                      <Form.Control
+                        type="date"
+                        name="enddate"
+                        placeholder="DateRange"
+                        value={enddate}
+                        onChange={(e) => setEndDate(e.target.value)}
+                      />
+                      {errors.enddate && (
+                        <p className="errorMsg">{errors.enddate.message}</p>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="row mb-3">
+                    <Form.Group controlId="designdetails">
                       <Form.Label>Design Details</Form.Label>
-                      <textarea name="designdetails" rows={3} cols={55} />
+                      <textarea name="designdetails" rows={3} cols={47} />
                       {errors.designdetails && (
                         <p className="errorMsg">
                           {errors.designdetails.message}
                         </p>
                       )}
                     </Form.Group>
-                    <div className="row mb-3">
-                      <div className="col-md-6">
-                        <Form.Label>Start date</Form.Label>
-                        <Form.Control
-                          type="date"
-                          name="startdate"
-                          placeholder="DateRange"
-                          value={startdate}
-                          onChange={(e) => setStartDate(e.target.value)}
+                  </div>
+                  <div className="row mb-3">
+                    <label>Status</label>
+                    <Controller
+                      name="status"
+                      control={control}
+                      render={({ onChange, value, ref }) => (
+                        <Select
+                          options={status}
+                          value={status.find((c) => c.value === value)}
+                          // onChange={(val) => onChange(val.value)}
                         />
-                        {errors.startdate && (
-                          <p className="errorMsg">{errors.startdate.message}</p>
-                        )}
-                      </div>
-
-                      <div className="col-md-6">
-                        <Form.Label>End date</Form.Label>
-                        <Form.Control
-                          type="date"
-                          name="enddate"
-                          placeholder="DateRange"
-                          value={enddate}
-                          onChange={(e) => setEndDate(e.target.value)}
-                        />
-                        {errors.enddate && (
-                          <p className="errorMsg">{errors.enddate.message}</p>
-                        )}
-                      </div>
-                    </div>
-                    <div className="mb-4">
-                      <label>Status</label>
-                      <Controller
-                        className="mb-3"
-                        name="status"
-                        control={control}
-                        render={({ onChange, value, ref }) => (
-                          <Select
-                            options={status}
-                            value={status.find((c) => c.value === value)}
-                            // onChange={(val) => onChange(val.value)}
-                          />
-                        )}
-                        rules={{ required: true }}
-                      />
-                    </div>
-
-                    <hr className="bg-danger border-2 border-top border-primary"></hr>
-
-                    <div className="text-center mt-3">
-                      <button
-                        type="submit"
-                        className="btn btn-outline-primary btn-rounded w-55"
-                      >
-                        Register Now
-                      </button>
-                    </div>
-                  </form>
+                      )}
+                      rules={{ required: true }}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
+              <div className="text-center mt-3">
+                <button
+                  type="submit"
+                  className="btn btn-outline-primary btn-rounded w-55"
+                >
+                  Register Now
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
